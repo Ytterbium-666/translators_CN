b1709b47d74d310a97dd9e5085b851273a7d0c15
HYb <1277325051@qq.com>
2022-09-15 20:08:33 +0800

    Debug

--- translators/Baidu Scholar.js
+++ translators/Baidu Scholar.js
@@ -38,6 +38,7 @@
 
 
 function getRefByIDs(ids, onDataAvailable) {
+	//I'm Yb
 	if (!ids.length) return;
 	let {url, paper} = ids.shift();
 	let refUrl = `https://xueshu.baidu.com/u/citation?type=bib&paperid=${paper}`;
